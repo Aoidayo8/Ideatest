039c9ff996ae564eae88e9bbaf6bab5c001684ad
aoidayo <aoidayo@163.com>
2022-12-01 12:23:43 +0800

    push test

--- src/test1/mytest1.java
+++ src/test1/mytest1.java
@@ -7,5 +7,6 @@ public class mytest1 {
         System.out.println("Hello git");
         System.out.println("Hello git");
         System.out.println("Hello git1");
+        System.out.println("push test");
     }
 }
