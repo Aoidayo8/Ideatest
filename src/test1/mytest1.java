ca2bb9f0384bbff8be6d60f347669695a22c47e2
aoidayo <aoidayo@163.com>
2022-12-01 10:48:16 +0800

    No amend1

--- src/test1/mytest1.java
+++ src/test1/mytest1.java
@@ -3,5 +3,7 @@ package test1;
 public class mytest1 {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
     }
 }
