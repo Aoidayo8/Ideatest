5c1cef8af0494396a7d91ff18d4eca4b9f7eddca
Aoidayo8 <119548430+Aoidayo8@users.noreply.github.com>
2022-12-01 12:26:12 +0800

    Update mytest1.java

--- src/test1/mytest1.java
+++ src/test1/mytest1.java
@@ -8,5 +8,6 @@ public class mytest1 {
         System.out.println("Hello git");
         System.out.println("Hello git1");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
